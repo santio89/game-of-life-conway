c03458b6b00a5c50fe132139280fb493ff55df4f
santio89 <santiolais1989@gmail.com>
2023-10-06 21:10:06 -0300

    few-styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ import Cell from "./Cell";
 export default function Home() {
     const [cells, setCells] = useState(null)
     const [cellsFilled, setCellsFilled] = useState(false)
-    const [cellSize, setCellSize] = useState(40)
+    const [cellSize, setCellSize] = useState(60)
     const [cols, setCols] = useState(Math.floor((window.innerWidth) / cellSize))
     const [rows, setRows] = useState(Math.floor((window.innerHeight - 60) / cellSize))
     const [playing, setPlaying] = useState(false)
@@ -215,24 +215,24 @@ export default function Home() {
         <div className="home">
             <div className="home__buttons">
                 <div className="home__buttons__box">
-                    <button className={`${playing && "active"}`} onClick={() => initGame()}>
+                    <button title="Play" className={`${playing && "active"}`} onClick={() => initGame()}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" className="bi bi-play-fill" viewBox="0 0 16 16">
                             <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
                         </svg>
                     </button>
-                    <button className={`${!playing && "active"}`} onClick={() => stopGame()}>
+                    <button title="Pause" className={`${!playing && "active"}`} onClick={() => stopGame()}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" className="bi bi-pause-fill" viewBox="0 0 16 16">
                             <path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5zm5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5z" />
                         </svg>
                     </button>
                 </div>
                 <div className="home__buttons__box">
-                    <button className={`${colorPlaying && "active"}`} onClick={() => toggleColorPlaying()}>
+                    <button title="Auto Colors" className={`${colorPlaying && "active"}`} onClick={() => toggleColorPlaying()}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-rainbow" viewBox="0 0 16 16">
                             <path d="M8 4.5a7 7 0 0 0-7 7 .5.5 0 0 1-1 0 8 8 0 1 1 16 0 .5.5 0 0 1-1 0 7 7 0 0 0-7-7zm0 2a5 5 0 0 0-5 5 .5.5 0 0 1-1 0 6 6 0 1 1 12 0 .5.5 0 0 1-1 0 5 5 0 0 0-5-5zm0 2a3 3 0 0 0-3 3 .5.5 0 0 1-1 0 4 4 0 1 1 8 0 .5.5 0 0 1-1 0 3 3 0 0 0-3-3zm0 2a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 4 0 .5.5 0 0 1-1 0 1 1 0 0 0-1-1z" />
                         </svg>
                     </button>
-                    <button onClick={() => calcRandomGen()}>
+                    <button title="Randomize" onClick={() => calcRandomGen()}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
                             <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z" />
                             <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z" />
