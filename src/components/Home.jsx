551b8bc86c94e84dd24c084508563aad765d8469
santio89 <santiolais1989@gmail.com>
2023-10-09 13:48:41 -0300

    adding-generation-buttons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -343,11 +343,21 @@ export default function Home({ rootTheme }) {
                                     <label htmlFor="boundRange">Bound</label>
                                     <input onChange={(e) => setBoundRange(e.target.value)} value={boundRange} id="boundRange" type="range" min="40" max="100" />
                                 </div>
+                                <div className="genBtns">
+                                    <button title="Previous generation" onClick={resetSettings}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left" viewBox="0 0 16 16">
+                                            <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
+                                        </svg>
+                                    </button>
+                                    <button title="Next generation" onClick={calcGen}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
+                                            <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                                        </svg>
+                                    </button>
+                                </div>
                                 <div className="clearBtns">
                                     <button title="Reset settings" onClick={resetSettings}>Reset</button>
-                                    <button title="Clear cells" onClick={() => {
-                                        fillArray()
-                                    }}>Clear</button>
+                                    <button title="Clear cells" onClick={fillArray}>Clear</button>
                                 </div>
                             </div>
                         }
