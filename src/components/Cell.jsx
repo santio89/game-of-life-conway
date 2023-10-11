597e4824b96590bd778548dcc832a8ad25d0043e
santio89 <santiolais1989@gmail.com>
2023-10-11 20:09:54 -0300

    small fix

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,7 +1,7 @@
 export default function Cell({ index, active, toggleActive, cellFillMove, gridMode }) {
 
   return (
-    <div aria-label={`${active ? "Cell: alive" : "Cell: dead"}`} className={`game-grid__cell ${active && "game-grid__cell--active"} ${gridMode && "gridMode"}`} onPointerDown={() => toggleActive(index)} onMouseEnter={() => cellFillMove(index)}  >
+    <div aria-label={`${active ? "Cell: alive" : "Cell: dead"}`} className={`game-grid__cell ${active && "game-grid__cell--active"} ${gridMode && "game-grid__cell--gridMode"}`} onPointerDown={() => toggleActive(index)} onMouseEnter={() => cellFillMove(index)}  >
       &nbsp;
     </div>
   )
